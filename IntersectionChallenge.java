ce81608ef3153448f79b973a5f8d6d6dd69f27cb
GideonDriver <gideondriver@gmail.com>
2022-05-02 17:37:39 -0400

    first commit - Gideon

--- IntersectionChallenge.java
+++ IntersectionChallenge.java
@@ -4,7 +4,7 @@ import java.util.*;
 // + In order to determine if two singly lists intersect, we need to know if any of the pointers are identical.
 // + This code compares each pointer from list1 against each pointer from list2 in order to identify any matches.
 
-// * I couldn't figure out how to get the reference numbers directly from the linked list, so I will use the values as if they are the pointers.
+// * I couldn't figure out how to get the reference numbers directly from the linked list, so I will use the values to store the IDs & Pointers.
 
 public class IntersectionChallenge
 {
@@ -18,10 +18,14 @@ public class IntersectionChallenge
 	        //iterate through the second list
 			for (int j = 0; j < list2.size(); j++) {
 				
+				//select pointers from String
+				String pointer1 = list1.get(i).substring(list1.get(i).indexOf("|")+1, list1.get(i).length());
+				String pointer2 = list2.get(j).substring(list2.get(j).indexOf("|")+1, list2.get(j).length());
+				
 				//compare each entry value*(should be pointer) in list1 against each entry value* in list2
-				if (list1.get(i).equals(list2.get(j))) {
+				if (pointer1.equals(pointer2)) {
 					//set variable "result" to the value where the intersection takes place
-					result = result.concat(spacer + list1.get(i));
+					result = result.concat(spacer + pointer1);
 					spacer = ",";
 				}
 			}
@@ -42,30 +46,31 @@ public class IntersectionChallenge
     	//Add two linked lists and populate them
         LinkedList<String> list1 = new LinkedList<String>();
         LinkedList<String> list2 = new LinkedList<String>();
-        
-        list1.add("A");
-        list1.add("B");
-        list1.add("C");
-        list1.add("D");
-        list1.add("E");
 
-        list2.add("D");
-        list2.add("G");
-        list2.add("H");
-        list2.add("E");
-        list2.add("J");
+        // * Organized as such: ID|Pointer
+        list1.add("A|B");
+        list1.add("B|C");
+        list1.add("C|D");
+        list1.add("D|E");
+        list1.add("E|F");
+
+        list2.add("F|D"); // F|G
+        list2.add("G|H");
+        list2.add("H|I");
+        list2.add("I|E"); // I|J
+        list2.add("J|K");
 
         //run the Intersection method to check for intersection between the lists
         System.out.println(Intersection(list1, list2));
         
         //Update list2 to remove duplicate value* "E"
-        list2.set(3, "I");
+        list2.set(3, "I|J");
 
         //run the Intersection method to check for intersection between the lists
         System.out.println(Intersection(list1, list2));
         
         //Update list2 to remove duplicate value* "D"
-        list2.set(0, "F");
+        list2.set(0, "F|G");
         
         //run the Intersection method to check for intersection between the lists
         System.out.println(Intersection(list1, list2));
